4c889e09cfbbfbbc771881f1b8b58e57ba77db04
Keith Kozma <kkozma@gmail.com>
2015-01-24 11:51:12 -0500

    Correct handling to disable QCOM_BSP
    Change-Id: I69a49ebea0f6ed7543f2fbf17fc3a34c90d9aede

--- core/qcom_target.mk
+++ core/qcom_target.mk
@@ -11,14 +11,26 @@ endef
 ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
 
     qcom_flags := -DQCOM_HARDWARE
+	
+	ifneq ($(BOARD_USES_LEGACY_QCOM_DISPLAY),true)
+        TARGET_USES_QCOM_BSP := true
+        TARGET_GLOBAL_CFLAGS += -DQCOM_BSP
+        TARGET_GLOBAL_CPPFLAGS += -DQCOM_BSP
+    endif
+	
     TARGET_ENABLE_QC_AV_ENHANCEMENTS := true
 
     # Enable DirectTrack for legacy targets
     ifneq ($(filter msm7x30 msm8660 msm8960,$(TARGET_BOARD_PLATFORM)),)
-        ifeq ($(BOARD_USES_LEGACY_ALSA_AUDIO),true)
-            qcom_flags += -DQCOM_DIRECTTRACK
-        endif
-        # Enable legacy graphics functions
+    ifeq ($(BOARD_USES_LEGACY_ALSA_AUDIO),true)
+        qcom_flags += -DQCOM_DIRECTTRACK
+    endif
+        ifneq ($(BOARD_USES_LEGACY_QCOM_DISPLAY),true)
+			# Enable legacy graphics functions
+			TARGET_USES_QCOM_BSP_LEGACY := true
+			TARGET_GLOBAL_CFLAGS += -DQCOM_BSP_LEGACY
+			TARGET_GLOBAL_CPPFLAGS += -DQCOM_BSP_LEGACY
+		endif
     endif
 
     TARGET_GLOBAL_CFLAGS += $(qcom_flags)
